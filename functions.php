27fc9b06632ff44a0de91f8ec2eb4e3d50338e07
Rafsan <kalilinux609@gmail.com>
2020-01-20 22:09:50 +0600

    initilize

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+
+// function loops()
+// {
+    
+//    Showing Piramid
+
+//     for( $i = 0; $i <= 5; $i++ )
+//     {
+//        for($x = 0; $x <= $i; $x++ )
+//        {
+//           echo $x;
+//        } 
+//        echo "<br>";
+//     }
+// }
+// $name = "mostafiz";
+// function boro_hater_lekha($name)
+// {
+//     echo ucwords($name);
+// }
+
